329f55b930596c9d9f6b0a85387fa8233d098750
Ahamed Aaqib <ahamedaaqib96@outlook.com>
2020-04-30 15:18:59 +0530

    Project Initialized

---
+++ EF-Adapter.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EF-Adapter.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EF-Adapter.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbf1788f-3d18-4180-9b85-c21b9b4dd075")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EF-Adapter.UnitTest/TestUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EF_Adapter.UnitTest
+{
+    [TestClass]
+    public class TestUtil
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

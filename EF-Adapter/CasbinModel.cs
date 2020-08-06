c2a3d9db4d59cff1b75041e8341dbb6d88b6112a
changethecode <jim-frey@web.de>
2020-08-06 17:57:56 +0200

    cleanup

--- EF-Adapter/CasbinModel.cs
+++ EF-Adapter/CasbinModel.cs
@@ -1,28 +1,26 @@
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
 using System;
-using System.Data.Entity.ModelConfiguration.Configuration;
 using Casbin.NET.Adapter.EF.Model;
 
 namespace Casbin.NET.Adapter.EF
 {
     public partial class CasbinDbContext<TKey> : DbContext where TKey : IEquatable<TKey>
     {
-        private readonly ConfigurationRegistrar _CasbinModelConfig;
         public virtual DbSet<CasbinRule<TKey>> CasbinRule { get; set; }
-        
+
 
         public CasbinDbContext(string databaseConnectionString)
             : this(databaseConnectionString, new ValidateDbInitializer<CasbinDbContext<TKey>>(), false)
         {
         }
 
-        public CasbinDbContext(string databaseConnectionString, EntityTypeConfiguration<CasbinDbContext<TKey>> entityType) 
-            : this (databaseConnectionString, new ValidateDbInitializer<CasbinDbContext<TKey>>(), false)
+        public CasbinDbContext(string databaseConnectionString, EntityTypeConfiguration<CasbinDbContext<TKey>> entityType)
+            : this(databaseConnectionString, new ValidateDbInitializer<CasbinDbContext<TKey>>(), false)
         {
         }
 
-        public CasbinDbContext(string databaseConnectionString, IDatabaseInitializer<CasbinDbContext<TKey>> initializer, bool forceInit = true) 
+        public CasbinDbContext(string databaseConnectionString, IDatabaseInitializer<CasbinDbContext<TKey>> initializer, bool forceInit = true)
             : base(databaseConnectionString)
         {
             Database.SetInitializer(initializer);
@@ -31,10 +29,7 @@ namespace Casbin.NET.Adapter.EF
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            if (_CasbinModelConfig != null)
-            {
-                modelBuilder.Entity<CasbinRule<TKey>>();
-            }
+            modelBuilder.Entity<CasbinRule<TKey>>();
         }
 
     }
